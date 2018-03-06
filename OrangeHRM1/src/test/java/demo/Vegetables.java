9afcfef7bd07a0117eafa4c130ffe075b0549841
User <User@User-PC>
2018-03-06 18:54:57 +0530

    code

--- OrangeHRM1/src/test/java/demo/Vegetables.java
+++ OrangeHRM1/src/test/java/demo/Vegetables.java
@@ -1,9 +1,25 @@
 package demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Vegetables {
-  @Test
-  public void f() {
+	
+  @Test(groups="Fruits")
+  public void Apple() {
+	  System.out.println("i am apple");
+  }
+  @Test(groups="Fruits")
+  public void Grapes() {
+	  System.out.println("Grapes");
+  }
+  @Test(groups="Vegetable")
+  public void tomato() {
+	  System.out.println("Tomato");
+  }
+  @Test(groups="Raja")
+  public void raj() {
+	  System.out.println("Raja is mad");
   }
 }
